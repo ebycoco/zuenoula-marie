097c4efc121132077954f633077fb7555b6880bd
Ebychoco <brouyaoeric7@gmail.com>
2021-06-09 02:21:33 +0000

    premier commit

--- config/bundles.php
+++ config/bundles.php
@@ -15,9 +15,9 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
-    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Eschmar\TimeAgoBundle\EschmarTimeAgoBundle::class => ['all' => true],
+    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
 ];
---
+++ src/Controller/Admin/AdminController.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/admin')]
+class AdminController extends AbstractController
+{
+    #[Route('/', name: 'admin_index', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('admin/dashboard.html.twig');
+    }
+}
